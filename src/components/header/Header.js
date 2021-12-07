c928d2556ca2d7229e2f27e80a17886f4297a9a7
sercan-works <sercantas1026@gmail.com>
2021-12-07 12:47:07 +0300

    first project

---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+
+import "./Header.css";
+
+const Header = () =>{
+    return(
+        <div className="big-title">
+            <h3>POPULAR TOUR PLACES</h3>
+        </div>
+
+    )
+}
+
+export default Header;
